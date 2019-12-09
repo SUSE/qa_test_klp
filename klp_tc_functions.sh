SUSE/qa_test_klp

commit 28279223239edfe3afa7cc8a60a15375f09d85b4

Fix 'mapfile: -d: invalid option' error
Mapfile option -d is available only with Bash 4.4 and newer.

Signed-off-by: Libor Pecháček <lpechacek@suse.cz>

klp_tc_functions.sh
@@ -315,7 +315,7 @@ if [ ! -f $KLP_ENV_CACHE_FILE ]; then
     echo -n 'export KLP_TEST_SYSCALL_FN_PREFIX=' >> $KLP_ENV_CACHE_FILE
 
     # generate LINUX_VERSION_CODE from `uname -r`
-    mapfile -d. -t VERSION_PARTS < <(uname -r | cut -d- -f1 )
+    mapfile -t VERSION_PARTS < <(uname -r | cut -d- -f1 | tr . ' ')
     VERSION_CODE=$(((VERSION_PARTS[0]<<16) + (VERSION_PARTS[1]<<8) + VERSION_PARTS[2]))
 
     if [ "$VERSION_CODE" -ge 266496 ] # test for kernel 4.17.0 and newer

commit 2ba02210cbe4ca218e9c93dec2139533b5495c42

Generate linux version code in a portable way
Commit 28279223239e ("Fix 'mapfile: -d: invalid option' error") introduced
breakage which is not detected by Bash errexit behavior. The symptom is the
following line in script output:

./klp_tc_functions.sh: line 320: 5 4 0: syntax error in expression (error token is "4 0")

The root cause is that single space does not serve as a field delimiter for
mapfile. At this point I stop experimenting with this Bash specific command and
adopt portable version suggested by Petr Vorel.

Fixes: 2827922 ("Fix 'mapfile: -d: invalid option' error")
Suggested-by: Petr Vorel <pvorel@suse.cz>
Signed-off-by: Libor Pecháček <lpechacek@suse.cz>

klp_tc_functions.sh
@@ -315,8 +315,11 @@ if [ ! -f $KLP_ENV_CACHE_FILE ]; then
     echo -n 'export KLP_TEST_SYSCALL_FN_PREFIX=' >> $KLP_ENV_CACHE_FILE
 
     # generate LINUX_VERSION_CODE from `uname -r`
-    mapfile -t VERSION_PARTS < <(uname -r | cut -d- -f1 | tr . ' ')
-    VERSION_CODE=$(((VERSION_PARTS[0]<<16) + (VERSION_PARTS[1]<<8) + VERSION_PARTS[2]))
+    KVER="$(uname -r | cut -d- -f1)"
+    PART1="$(echo $KVER | cut -d. -f1)"
+    PART2="$(echo $KVER | cut -d. -f2)"
+    PART3="$(echo $KVER | cut -d. -f3)"
+    VERSION_CODE=$(((PART1 <<16) + (PART2 <<8) + PART3))
 
     if [ "$VERSION_CODE" -ge 266496 ] # test for kernel 4.17.0 and newer
     then